1ce45420db2a8befde9699f7fab3e7e1b881d97d
cbrognara <csbrognara@gmail.com>
2023-08-23 18:29:19 -0300

    initi

---
+++ README.md
@@ -0,0 +1,25 @@
+# React + TypeScript + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Using as Tools:
+
+- [] Vite + TypeScript
+- [] Styled components
+- [] Radix UI
+- [] Json-server
+- [] React-router-dom
+- [] Phosphor icons
+- [] React-hook-form
+- [] Zod
+- [] Axios
+
+## Expanding the ESLint configuration
+
+Start with:
+
+```
+npm i
+npm run dev
+npx json-server server.json -p 3333
+```
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from '../../assets/ignite-logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewtransactionModal } from "../NewTransactionModal";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} alt='' />
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewtransactionModal />
+                </Dialog.Root>
+
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
